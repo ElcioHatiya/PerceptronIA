940f3ec09c8a3cf49730c8790251d71f7a9508b6
elcio <elciohatiya@outlook.com>
2023-08-28 07:48:22 -0300

    Tela completa

--- app/src/main/java/com/example/perceptronia/MainActivity.kt
+++ app/src/main/java/com/example/perceptronia/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.perceptronia
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.perceptronia.databinding.ActivityMainBinding
 
@@ -14,26 +15,57 @@ class MainActivity : ComponentActivity() {
         o=ActivityMainBinding.inflate(layoutInflater)
         setContentView(o.root)
 
-        var a1=Integer.valueOf(o.spnA1.text.toString())
-        var a2=Integer.valueOf(o.spnA2.text.toString())
-        var a3=Integer.valueOf(o.spnA3.text.toString())
-        var b1=Integer.valueOf(o.spnB1.text.toString())
-        var b2=Integer.valueOf(o.spnB2.text.toString())
-        var b3=Integer.valueOf(o.spnB3.text.toString())
-        var c1=Integer.valueOf(o.spnC1.text.toString())
-        var c2=Integer.valueOf(o.spnC2.text.toString())
-        var c3=Integer.valueOf(o.spnC3.text.toString())
-        var d1=Integer.valueOf(o.spnD1.text.toString())
-        var d2=Integer.valueOf(o.spnD2.text.toString())
-        var d3=Integer.valueOf(o.spnD3.text.toString())
+        o.btnCalc.setOnClickListener {
 
-        var bias = 1
-        var peso = 0
+            var a1= o.spnA1.text.toString()
+            var a2= o.spnA2.text.toString()
+            var Sa3= o.spnA3.text.toString()
+            var peso1 = o.peso1.text.toString()
+            var b1= o.spnB1.text.toString()
+            var b2= o.spnB2.text.toString()
+            var Sb3= o.spnB3.text.toString()
+            var peso2 = o.peso2.text.toString()
+            var c1= o.spnC1.text.toString()
+            var c2= o.spnC2.text.toString()
+            var Sc3= o.spnC3.text.toString()
+            var peso3 = o.peso3.text.toString()
+            var d1= o.spnD1.text.toString()
+            var d2= o.spnD2.text.toString()
+            var Sd3= o.spnD3.text.toString()
+            var peso4 = o.peso4.text.toString()
 
-        fun calcularPeso(x1:Int,x2:Int,peso:Int):Int{
-            var S = ((bias*peso)+(peso*x1)+(peso*x2))
-            var erro = (a3-S)
-            return S
+            var bias = 1
+            var taxaAprendizagem = 1
+
+            fun calcularSaida(x1:Int,x2:Int,peso:Int):Int{
+                var S = ((Integer.valueOf(peso)*bias)+(peso*x1)+(peso*x2))
+                return S
+            }
+
+            //Verifica se o Resultado é o mesmo que o Esperado
+            fun verificarERRO(result:Int, esperado:Int):Int{
+                if(result==esperado){return 0}
+                else{return 1}
+            }
+
+            //Compara resultado com a saída esperada
+            fun verificaResult(result:Int,saida:Int):Int{
+                if(result==saida) return 0
+                else return 1
+            }
+
+            if(a1.isEmpty()||a2.isEmpty()||Sa3.isEmpty()||b1.isEmpty()||b2.isEmpty()||Sb3.isEmpty()||
+               c1.isEmpty()||c2.isEmpty()||Sc3.isEmpty()||d1.isEmpty()||d2.isEmpty()||Sd3.isEmpty())
+                Toast.makeText(this@MainActivity,"Preencha todos os campos!", Toast.LENGTH_LONG).show()
+
+            else{
+                var A1=Integer.valueOf(a1)
+                var A2=Integer.valueOf(a2)
+                var A3=Integer.valueOf(Sa3)
+                var P1=Integer.valueOf(peso1)
+                var calc = calcularSaida(A1,A2,P1)
+                Toast.makeText(this@MainActivity,calc.toString(), Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
